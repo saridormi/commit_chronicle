07616740e55a1c1c52b0f374d601d0fa49177f9c
Aleksandra Eliseeva <saridormi123@gmail.com>
2021-10-15 11:32:29 +0300

    Change data collection script to process only specified hashes

--- collect_data.py
+++ collect_data.py
@@ -1,4 +1,5 @@
 import os
+import json
 import csv
 import gzip
 import argparse
@@ -6,6 +7,7 @@ import pydriller
 import logging
 from typing import List
 from joblib import Parallel, delayed
+from configparser import NoOptionError
 from commit_processor import CommitProcessor
 
 logging.basicConfig(
@@ -17,38 +19,37 @@ logging.basicConfig(
 )
 
 
-def process_repo(repos_dir, commit_data_dir, repo_name, repo_url, file_types):
+def process_repo(temp_clone_dir, output_dir, full_repo_name, repo_url, hashes):
     """
     Download author, date, diff and message of all .java-related commits
     and save to .jsonl.gz
     """
-    os.makedirs(repos_dir, exist_ok=True)
-    os.makedirs(os.path.join(commit_data_dir, repo_name.split("/")[0]), exist_ok=True)
+    org_name, repo_name = full_repo_name.split("/", 1)
+    os.makedirs(temp_clone_dir, exist_ok=True)
+    os.makedirs(os.path.join(output_dir, org_name, repo_name), exist_ok=True)
 
-    if repo_name.split("/")[1] + ".csv.gz" in os.listdir(os.path.join(commit_data_dir, repo_name.split("/")[0])):
-        logging.warning(f" [{repo_name}] Skipping, already processed")
+    if "commits.csv.gz" in os.listdir(os.path.join(output_dir, org_name, repo_name)):
         return
 
     # do not clone repos that are already cloned
-    if repo_url.split("/")[-1].replace(".git", "") in os.listdir(repos_dir):
-        logging.warning(f" [{repo_name}] Already cloned")
+    if repo_url.split("/")[-1].replace(".git", "") in os.listdir(temp_clone_dir):
+        logging.warning(f"[{repo_name}] Already cloned")
         repo = pydriller.RepositoryMining(
-            f'{repos_dir}/{repo_url.split("/")[-1].replace(".git", "")}',
-            only_no_merge=True,
-            only_modifications_with_file_types=file_types,
+            f'{temp_clone_dir}/{repo_url.split("/")[-1].replace(".git", "")}', only_no_merge=True, only_commits=hashes
         )
     else:
+        logging.warning(f"[{repo_name}] Cloning...")
         try:
             repo = pydriller.RepositoryMining(
-                repo_url, clone_repo_to=repos_dir, only_no_merge=True, only_modifications_with_file_types=file_types
+                repo_url, clone_repo_to=temp_clone_dir, only_no_merge=True, only_commits=hashes
             )
         except:  # sometimes git errors can happen during cloning
             logging.error(f"Couldn't clone {repo_url}")
             return
 
-    logging.warning(f" [{repo_name}] Start processing")
+    logging.warning(f"[{repo_name}] Start processing; {len(hashes)} commits to go")
 
-    csv_filename = os.path.join(commit_data_dir, repo_name.split("/")[0], repo_name.split("/")[1] + ".csv")
+    csv_filename = os.path.join(output_dir, org_name, repo_name, "commits.csv")
     fieldnames = ["author", "date", "hash", "message", "diff"]
     with open(csv_filename, "w") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -57,20 +58,27 @@ def process_repo(repos_dir, commit_data_dir, repo_name, repo_url, file_types):
     commits_data = []
 
     for commit in repo.traverse_commits():
-        cur_data = CommitProcessor.process_commit(commit)
+        try:
+            cur_data = CommitProcessor.process_commit(commit)
+        except AttributeError:
+            logging.error(f"AttributeError with {org_name}/{repo_name}")
+            continue
+        except NoOptionError:
+            logging.error(f"NoOptionError with {org_name}/{repo_name}")
+            continue
 
         if filter_diff(cur_data["diff"]) and filter_msg(cur_data["message"]):
             commits_data.append(cur_data)
 
         if len(commits_data) >= 1000:
-            logging.warning(f" [{repo_name}] Processed more than 1000 commits, writing to file")
+            logging.warning(f"[{repo_name}] Processed more than 1000 commits, writing to file")
             with open(csv_filename, "a") as csvfile:
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                 writer.writerows(commits_data)
                 commits_data = []
 
     if len(commits_data) > 0:
-        logging.warning(f" [{repo_name}] Final writing to file")
+        logging.warning(f"[{repo_name}] Final writing to file")
         with open(csv_filename, "a") as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerows(commits_data)
@@ -79,7 +87,7 @@ def process_repo(repos_dir, commit_data_dir, repo_name, repo_url, file_types):
         f_out.writelines(f_in)
     os.remove(csv_filename)
 
-    logging.warning(f" [{repo_name}] Finish processing")
+    logging.warning(f"[{repo_name}] Finish processing")
 
 
 def filter_diff(diff: List[str], min_len=1) -> bool:
@@ -101,37 +109,27 @@ if __name__ == "__main__":
         description="This script collects commit data from provided list of GitHub repos.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("--repos_dir", type=str, default="temp", help="path to directory to clone repos to")
-    parser.add_argument(
-        "--commit_data_dir",
-        type=str,
-        default="extracted_data_csv",
-        help="path to directory to save collected commit data",
-    )
-    parser.add_argument("--repos_urls_file", type=str, default="repos_urls.txt", help="path to file with repos urls")
-    parser.add_argument("--repos_names_file", type=str, default="repos_names.txt", help="path to file with repos names")
-    parser.add_argument(
-        "--file_types",
-        type=List[str],
-        default=[".py"],
-        help="only analyses commits in which at least one modification was done in provided file types",
-    )
+
+    parser.add_argument("--temp_clone_dir", type=str, default="temp", help="path to directory to clone repos to")
+    parser.add_argument("--output_dir", type=str, help="path to directory to save collected commit data")
+
+    parser.add_argument("--input_root_dir", type=str, help="path to root folder with repos info")
+
     args = parser.parse_args()
 
-    with open(args.repos_urls_file, "r") as file:
-        repo_urls_list = [line.strip() for line in file.readlines()]
+    inputs = []
+    for repo in os.listdir(args.input_root_dir):
+        with open(os.path.join(args.input_root_dir, repo), "r") as infile:
+            inputs.append(json.load(infile))
 
-    with open(args.repos_names_file, "r") as file:
-        repo_names_list = [line.strip() for line in file.readlines()]
-
-    with Parallel(4) as pool:
+    with Parallel(8) as pool:
         pool(
             delayed(process_repo)(
-                repos_dir=args.repos_dir,
-                commit_data_dir=args.commit_data_dir,
-                repo_name=repo_name,
-                repo_url=repo_url,
-                file_types=args.file_types,
+                temp_clone_dir=args.temp_clone_dir,
+                output_dir=args.output_dir,
+                full_repo_name=cur_input["repo"],
+                repo_url=cur_input["url"],
+                hashes=cur_input["hashes"],
             )
-            for repo_name, repo_url in zip(repo_names_list, repo_urls_list)
+            for cur_input in inputs
         )
