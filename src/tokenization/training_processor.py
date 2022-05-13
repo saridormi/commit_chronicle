434c0fd5c95705c9009ab1729065d7699711e49e
Alexandra Eliseeva <saridormi123@gmail.com>
2022-05-13 19:01:31 +0300

    Update data tokenization script
    * switch to csv df instead of dask (it fits into memory and runs wayy faster)
    * support using both custom tokenizer and tokenizer from huggingface hub for diffs and messages
    * update configuration: do not truncate sequences by default

--- src/tokenization/training_processor.py
+++ src/tokenization/training_processor.py
@@ -4,6 +4,7 @@ import os
 from typing import Any, Dict, List, Optional
 
 import jsonlines
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from transformers import AutoTokenizer, PreTrainedTokenizerFast
@@ -18,13 +19,12 @@ class TrainingProcessor(BaseProcessor):
 
     * Construct commit message history for each author
     * Tokenize diffs and messages
-    * Save everything in format required by training pipeline
+    * Save everything in format required by commit message completion training pipeline
         (https://github.com/JetBrains-Research/commit_message_generation)
 
     Args:
-        diff_tokenizer_path: Path to diff tokenizer file.
-        msg_tokenizer_name: Model name on HuggingFace hub.
-        blocksize: Number of bytes in a single partition, used by dask.
+        diff_tokenizer_name_or_path: Path to tokenizer file or model name on HuggingFace hub for diffs.
+        msg_tokenizer_name_or_path: Path to tokenizer file or model name on HuggingFace hub for messages
         **diff_kwargs: Keyword arguments for diff tokenizer's __call__ method.
         **msg_kwargs: Keyword arguments for message tokenizer's __call__ method.
         data_format: In which format mined data is saved.
@@ -35,9 +35,8 @@ class TrainingProcessor(BaseProcessor):
 
     def __init__(
         self,
-        diff_tokenizer_path: str,
-        msg_tokenizer_name: str,
-        blocksize: int,
+        diff_tokenizer_name_or_path: str,
+        msg_tokenizer_name_or_path: str,
         clean_temp_files: bool,
         diff_kwargs: Dict[str, Any],
         msg_kwargs: Dict[str, Any],
@@ -47,9 +46,17 @@ class TrainingProcessor(BaseProcessor):
         logger_name: Optional[str] = None,
     ):
         super().__init__(chunksize=chunksize, logger_name=logger_name, n_workers=n_workers, data_format=data_format)
-        self._diff_tok = PreTrainedTokenizerFast(tokenizer_file=diff_tokenizer_path)
-        self._msg_tok = AutoTokenizer.from_pretrained(msg_tokenizer_name)
-        self._blocksize = blocksize
+
+        if ".json" in diff_tokenizer_name_or_path:
+            self._diff_tok = PreTrainedTokenizerFast(tokenizer_file=diff_tokenizer_name_or_path)
+        else:
+            self._diff_tok = AutoTokenizer.from_pretrained(diff_tokenizer_name_or_path, use_fast=True)
+
+        if ".json" in msg_tokenizer_name_or_path:
+            self._msg_tok = PreTrainedTokenizerFast(tokenizer_file=msg_tokenizer_name_or_path)
+        else:
+            self._msg_tok = AutoTokenizer.from_pretrained(msg_tokenizer_name_or_path, use_fast=True)
+
         self._clean_temp_files = clean_temp_files
         self._diff_kwargs = diff_kwargs
         self._msg_kwargs = msg_kwargs
@@ -71,32 +78,49 @@ class TrainingProcessor(BaseProcessor):
             Assumes that commits from each author are already in correct chronological order,
             which is true for default PyDriller configuration.
         """
-        df = self._read_input(in_fname, read_lazy=True, blocksize=self._blocksize)
+        reader = self._read_input(in_fname)
+        for i, chunk in enumerate(tqdm(reader, desc=f"Iterating over {part} to save necessary columns to csv")):
+            if i == 0:
+                chunk[["author", "diff", "message"]].to_csv(
+                    os.path.join(output_dir, f"temp_{part}.csv"), index=None, header=True, mode="w"
+                )
+            else:
+                chunk[["author", "diff", "message"]].to_csv(
+                    os.path.join(output_dir, f"temp_{part}.csv"), index=None, header=None, mode="a"
+                )
+
+        self.logger.info("Reading csv df")
+        df = pd.read_csv(os.path.join(output_dir, f"temp_{part}.csv"), lineterminator="\n")
+        self.logger.info("Aggregating history positions")
         df["pos_in_history"] = df.groupby("author").cumcount()
 
         if part == "train":
+            self.logger.info("Shuffling train df")
             df = df.sample(frac=1.0, random_state=123)
 
-        self._prepare_outfile(os.path.join(output_dir, f"temp_{part}"))
-
-        for partition in tqdm(df.partitions, desc=f"Saving data in blocks (~{self._blocksize * 1e-7} Mb)"):
-            partition = partition.compute()
-            partition["date"] = partition["date"].astype(str)
-            self._append_to_outfile(partition, os.path.join(output_dir, f"temp_{part}"))
+        self.logger.info("Saving csv df")
+        df.to_csv(os.path.join(output_dir, f"temp_{part}.csv"), index=None, header=True, mode="w")
 
     def _process_messages(self, output_dir: str, part: str) -> None:
-        """Tokenizes messages, constructs commit message history for each author and saves to separate file.
-        """
-        reader = self._read_input(os.path.join(output_dir, f"temp_{part}"))
+        """Tokenizes messages, constructs commit message history for each author and saves to separate file."""
+        reader = pd.read_csv(
+            os.path.join(output_dir, f"temp_{part}.csv"),
+            usecols=["message", "author"],
+            dtype={"author": np.int32, "message": str},
+            chunksize=self._chunksize,
+            lineterminator="\n",
+        )
 
         open(os.path.join(output_dir, f"msgs_{part}.jsonl"), mode="w").close()
         for chunk in tqdm(reader, desc=f"Tokenizing messages in chunks ({self._chunksize} rows)"):
-            msg_input_ids = self._tokenize_messages(chunk["message"].tolist())
-            chunk["msg_input_ids"] = msg_input_ids
-
+            try:
+                chunk["msg_input_ids"] = self._tokenize_messages(chunk["message"].tolist())
+            except TypeError:
+                chunk["msg_input_ids"] = self._tokenize_messages([str(msg) for msg in chunk["message"].tolist()])
             with jsonlines.open(os.path.join(output_dir, f"msgs_{part}.jsonl"), mode="a") as writer:
                 for row in chunk[["msg_input_ids", "author"]].to_dict(orient="records"):
                     writer.write(row)
+
         df = pd.read_json(os.path.join(output_dir, f"msgs_{part}.jsonl"), orient="records", lines=True)
 
         history = df[["author", "msg_input_ids"]].groupby("author").agg(list)["msg_input_ids"].to_dict()
@@ -107,16 +131,20 @@ class TrainingProcessor(BaseProcessor):
         """Tokenizes diffs and saves all necessary information for working with commit message history
         to separate file.
         """
-        reader = self._read_input(os.path.join(output_dir, f"temp_{part}"))
+        reader = pd.read_csv(
+            os.path.join(output_dir, f"temp_{part}.csv"),
+            dtype={"author": np.int32, "pos_in_history": np.int32, "diff": str},
+            usecols=["diff", "author", "pos_in_history"],
+            chunksize=self._chunksize,
+            lineterminator="\n",
+        )
         open(os.path.join(output_dir, f"{part}.json"), mode="w").close()
 
         for chunk in tqdm(reader, desc=f"Tokenizing diffs in chunks ({self._chunksize} rows)"):
-            diff_input_ids = self._tokenize_diffs(chunk["diff"].tolist())
-            chunk["diff_input_ids"] = diff_input_ids
+            chunk["diff_input_ids"] = self._tokenize_diffs(chunk["diff"].tolist())
 
             with jsonlines.open(os.path.join(output_dir, f"{part}.json"), mode="a") as writer:
-                for row in chunk[["diff_input_ids", "pos_in_history", "author"]].to_dict(orient="records"):
-                    writer.write(row)
+                writer.write_all(chunk[["diff_input_ids", "pos_in_history", "author"]].to_dict(orient="records"))
 
     def __call__(self, in_fname: str, output_dir: str, part: str, **kwargs) -> None:
         """This method processes data into format required by our pipeline for training & evaluation
@@ -141,5 +169,5 @@ class TrainingProcessor(BaseProcessor):
         logging.info(f"Finish processing {part}")
 
         if self._clean_temp_files:
-            os.remove(os.path.join(output_dir, f"temp_{part}.{self.data_format}"))
+            os.remove(os.path.join(output_dir, f"temp_{part}.csv"))
             os.remove(os.path.join(output_dir, f"msgs_{part}.jsonl"))
--- src/tokenize_data.py
+++ src/tokenize_data.py
@@ -12,22 +12,27 @@ from .tokenization import TrainingProcessor
 def main(cfg: DictConfig) -> None:
     for key in cfg.paths:
         cfg.paths[key] = to_absolute_path(cfg.paths[key])
+
+    for key in ["diff_tokenizer_name_or_path", "msg_tokenizer_name_or_path"]:
+        if ".json" in cfg.training_processor[key]:
+            cfg.training_processor[key] = to_absolute_path(cfg.training_processor[key])
+
     os.makedirs(cfg.paths.output_dir, exist_ok=True)
 
     parts = ["train"] + sorted(
         [
             part.split(".")[0]
             for part in os.listdir(cfg.paths.input_dir)
-            if not os.path.isdir(os.path.join(cfg.paths.input_dir, part)) and "train" not in part
+            if not os.path.isdir(os.path.join(cfg.paths.input_dir, part))
+            and "train" not in part
+            and "final" not in part
         ]
     )
 
     logging.info("======= Using config =======")
     logging.info(cfg)
 
-    processor = TrainingProcessor(
-        **cfg.training_processor, diff_tokenizer_path=cfg.paths.diff_tokenizer_path, data_format=cfg.data_format
-    )
+    processor = TrainingProcessor(**cfg.training_processor, data_format=cfg.data_format)
     for part in parts:
         processor(
             in_fname=os.path.join(cfg.paths.input_dir, "tokenization", f"{part}_final"),
